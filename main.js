1111419bffc0d52dcd0ab2dc3d05d845ab16cc6f
jiianiii <38034156+jiianiii@users.noreply.github.com>
2018-05-13 22:27:41 +0900

    0513

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 $(function(){ 
     var duration=300; 
     
-    var $aside = $('.page-main > aside');
+   // var $aside = $('.page-main > aside'); 를 아래와 같이 변경
+    
+    var $aside = $('aside');
     var $asidButton= $aside.find('button')
         .on('click',function(){
             $aside.toggleClass('open');
@@ -21,6 +23,7 @@ $(function(){
 });
 
 
+//메뉴 hide /show
             $(document).ready(function() {
             
             //[2] more...클릭시 보이기 및 숨기기 토글
@@ -47,6 +50,59 @@ $(function(){
             $("span.moreOceania").click(function() {
                 console.log("오세아니아");
                 $("#regionOceania").toggle('2000'); 
-               
             });
+     
+            $("#table").click(function(){
+                
+                $("#contents").load("table.html");});
+                     
+        
         });
+
+
+//검색창
+$(function(){
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("search");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+// When the user clicks the button, open the modal 
+$('.content').click(function() {
+    modal.style.display = "block";
+    console.log("검색창");
+});
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    };
+
+};
+});
+
+//게시판
+
+
+//클맀했을떄, contents화면 이동
+ $(document).ready(function(){
+
+        $("#trip1").click(function(){
+          $("#contents").load("london.html");});
+     
+        $("#trip2").click(function(){
+          $("#contents").load("Barcelona.html");});  
+     
+});
+
+
